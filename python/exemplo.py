3f0155c2bb246500c57d428a292932842baf8a3d
Lucas Frederico Roeder de Mello <104468335+lucasmellofred@users.noreply.github.com>
2023-07-07 20:25:35 -0300

    Update exemplo.py

--- python/exemplo.py
+++ python/exemplo.py
@@ -1,16 +1,22 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 
 def montar_computador():
     def exibir_recomendacao():
         try:
             # Obter valores das respostas
-            orcamento = float(var_orcamento.get())
-            uso = var_uso.get()
-            area = var_area.get()
+            orcamento_texto = combo_orcamento.get()
+            if orcamento_texto.startswith("Até"):
+                orcamento = float(orcamento_texto[orcamento_texto.find("R$")+2:])
+            else:
+                orcamento = float(orcamento_texto[orcamento_texto.find("R$")+2:orcamento_texto.find(" -")])
+
+            uso = combo_uso.get()
+            area = combo_area.get()
 
             # Definir as peças com base nas respostas
-            if orcamento < 1000:
+            if orcamento == 1000:
                 processador = "Intel Core i3 ou equivalente"
                 placa_mae = "Placa-mãe básica"
                 memoria_ram = "8 GB de memória RAM (2x4GB)"
@@ -18,7 +24,7 @@ def montar_computador():
                 fonte = "Fonte de alimentação de 400W"
                 gabinete = "Gabinete básico"
                 placa_video = "Placa de vídeo integrada"
-            elif orcamento < 1500:
+            elif orcamento == 1500:
                 processador = "Intel Core i5 ou equivalente"
                 placa_mae = "Placa-mãe intermediária"
                 memoria_ram = "16 GB de memória RAM (2x8GB)"
@@ -51,6 +57,31 @@ def montar_computador():
                 gabinete = "Gabinete de alto desempenho com recursos avançados de refrigeração e personalização"
                 placa_video = "Placa de vídeo de alto desempenho para aplicações profissionais"
 
+            # Verificar uso selecionado para ajustar recomendações
+            uso = combo_uso.get()
+            if uso == "Doméstico":
+                if orcamento > 1500:
+                    memoria_ram = "16 GB de memória RAM (2x8GB)"
+                    armazenamento = "SSD de 480GB"
+            elif uso == "Jogos":
+                if orcamento > 2500:
+                    processador = "Intel Core i9 ou equivalente"
+                    placa_mae = "Placa-mãe avançada"
+                    memoria_ram = "32 GB de memória RAM (2x16GB)"
+                    armazenamento = "SSD de 1 TB + HD de 2 TB"
+                    fonte = "Fonte de alimentação de 600W"
+                    gabinete = "Gabinete avançado com boa refrigeração"
+                    placa_video = "Placa de vídeo dedicada de médio desempenho"
+            elif uso == "Profissional":
+                if orcamento > 5000:
+                    processador = "Intel Core i9-10900K ou equivalente"
+                    placa_mae = "Placa-mãe compatível com o processador"
+                    memoria_ram = "128 GB de memória RAM (4x32GB)"
+                    armazenamento = "SSD de 2 TB + HD de 8 TB"
+                    fonte = "Fonte de alimentação de 1000W"
+                    gabinete = "Gabinete de alto desempenho com recursos avançados de refrigeração e personalização"
+                    placa_video = "Placa de vídeo de alto desempenho para aplicações profissionais"
+
             # Exibir as peças recomendadas na mesma janela
             resposta = "Recomendação:\n\n" \
                        f"Processador: {processador}\n" \
@@ -68,9 +99,9 @@ def montar_computador():
 
     def reiniciar():
         # Limpar as respostas
-        var_orcamento.set(None)
-        var_uso.set(None)
-        var_area.set("")
+        combo_orcamento.set("")
+        combo_uso.set("")
+        combo_area.set("")
 
         label_resposta.config(text="")
 
@@ -94,38 +125,59 @@ def montar_computador():
     label_orcamento = tk.Label(janela, text="Qual é o seu orçamento?")
     label_orcamento.pack()
 
-    var_orcamento = tk.StringVar()
     opcoes_orcamento = [
-        ("Até R$ 1000", 1000),
-        ("R$ 1000 - R$ 1500", 1500),
-        ("R$ 1500 - R$ 2500", 2500),
-        ("R$ 2500 - R$ 5000", 5000),
-        ("Mais de R$ 5000", 10000)
+        "Até R$ 1000",
+        "R$ 1000 - R$ 1500",
+        "R$ 1500 - R$ 2500",
+        "R$ 2500 - R$ 5000",
+        "Mais de R$ 5000"
     ]
-    for texto, valor in opcoes_orcamento:
-        rb_orcamento = tk.Radiobutton(janela, text=texto, variable=var_orcamento, value=valor)
-        rb_orcamento.pack(anchor="w")
+
+    combo_orcamento = ttk.Combobox(janela, values=opcoes_orcamento)
+    combo_orcamento.pack()
+    combo_orcamento.configure(width=40)
 
     label_uso = tk.Label(janela, text="Qual será o uso principal do computador?")
     label_uso.pack()
 
     var_uso = tk.StringVar()
     opcoes_uso = [
-        ("Uso doméstico", "Doméstico"),
-        ("Uso para jogos", "Jogos"),
-        ("Uso profissional (edição de vídeo, design gráfico, etc.)", "Profissional"),
-        ("Uso para programação e desenvolvimento", "Programação")
+        ("Doméstico"),
+        ("Jogos"),
+        ("Profissional"),
+        ("Programação")
     ]
-    for texto, valor in opcoes_uso:
-        rb_uso = tk.Radiobutton(janela, text=texto, variable=var_uso, value=valor)
-        rb_uso.pack(anchor="w")
-
+    combo_uso = ttk.Combobox(janela, values=opcoes_uso)
+    combo_uso.pack()
+    combo_uso.configure(width=40) 
+    
     label_area = tk.Label(janela, text="Qual é a sua área de atuação? (opcional)")
-    label_area.pack()
-    entry_area = tk.Entry(janela)
-    entry_area.pack()
+    label_area.pack()    
     var_area = tk.StringVar()
-
+    opcoes_area = [
+        ("Design, Fotografia e Videomaker"),
+        ("Arquitetura e Engenharias"),
+        ("Odontologia"),
+        ("Engenharia"),
+        ("Ciências da Computação"),
+        ("Marketing e Publicidade"),
+        ("Finanças e Contabilidade"),
+        ("Ciências Biológicas"),
+        ("Medicina e Saúde"),
+        ("Educação e Pedagogia"),
+        ("Direito"),
+        ("Ciências Sociais e Humanas"),
+        ("Gestão de Projetos"),
+        ("Consultoria"),
+        ("Turismo e Hotelaria"),
+        ("Comércio e Vendas"),
+        ("Logística e Supply Chain"),
+        ("Música e Artes Cênicas")
+    ]
+    combo_area = ttk.Combobox(janela, values=opcoes_area)
+    combo_area.pack()
+    combo_area.configure(width=40) 
+    
     botao_confirmar = tk.Button(janela, text="Confirmar", command=exibir_recomendacao)
     botao_confirmar.pack()
 
