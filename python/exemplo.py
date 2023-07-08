8b3c2a1f2a2e0fc13e46b78f1c3e5ecf2de8f575
Lucas Frederico Roeder de Mello <104468335+lucasmellofred@users.noreply.github.com>
2023-07-07 21:48:09 -0300

    Update exemplo.py

--- python/exemplo.py
+++ python/exemplo.py
@@ -7,7 +7,7 @@ def montar_computador():
         try:
             # Obter valores das respostas
             orcamento_texto = combo_orcamento.get()
-            if orcamento_texto.startswith("Até"):
+            if orcamento_texto.startswith("Acima"):
                 orcamento = float(orcamento_texto[orcamento_texto.find("R$")+2:])
             else:
                 orcamento = float(orcamento_texto[orcamento_texto.find("R$")+2:orcamento_texto.find(" -")])
@@ -16,51 +16,67 @@ def montar_computador():
             area = combo_area.get()
 
             # Definir as peças com base nas respostas
-            if orcamento == 1000:
-                processador = "Intel Core i3 ou equivalente"
-                placa_mae = "Placa-mãe básica"
+            if orcamento == 2000: #Opção acima de R$ 2000
+                processador = "Ryzen 5 4600G"
+                placa_mae = "Placa-mãe A520"
                 memoria_ram = "8 GB de memória RAM (2x4GB)"
-                armazenamento = "SSD de 240GB"
-                fonte = "Fonte de alimentação de 400W"
-                gabinete = "Gabinete básico"
-                placa_video = "Placa de vídeo integrada"
-            elif orcamento == 1500:
-                processador = "Intel Core i5 ou equivalente"
-                placa_mae = "Placa-mãe intermediária"
-                memoria_ram = "16 GB de memória RAM (2x8GB)"
-                armazenamento = "SSD de 480GB"
-                fonte = "Fonte de alimentação de 500W"
-                gabinete = "Gabinete intermediário"
-                placa_video = "Placa de vídeo dedicada de entrada"
-            elif orcamento < 2500:
-                processador = "Intel Core i7 ou equivalente"
+                armazenamento = "SSD M.2 NVMe 512GB"
+                fonte = "Fonte de alimentação de 450W"
+                placa_video = "Placa de vídeo é integrada nesse processador"
+            elif orcamento < 2500: #Opção R$ 2001 - R$ 2500
+                processador = "Intel Core i3 10100F "
+                placa_mae = "Placa-mãe A320"
+                memoria_ram = "Memória RAM 2x8GB DDR4 @3200MHz"
+                armazenamento = "SSD M.2 NVMe 512GB"
+                fonte = "Fonte de 550W"
+                placa_video = "GTX 1650 ou RX 6400"
+            elif orcamento < 3000: #Opcão R$ 2500 - R$ 3000
+                processador = "Ryzen 5 5500"
                 placa_mae = "Placa-mãe intermediária ou avançada"
                 memoria_ram = "32 GB de memória RAM (2x16GB)"
-                armazenamento = "SSD de 1 TB + HD de 2 TB"
+                armazenamento = "SSD NVMe 1TB Kingston NV2"
                 fonte = "Fonte de alimentação de 600W"
-                gabinete = "Gabinete avançado com boa refrigeração"
-                placa_video = "Placa de vídeo dedicada de médio desempenho"
-            elif orcamento < 5000:
-                processador = "Intel Core i9 ou equivalente"
+                placa_video = "RX 6600 ou RTX 2060 ou  RTX 3050"
+            elif orcamento < 4000: #Opção R$ 3001 - R$ 4000
+                processador = "Ryzen 5 5500"
+                placa_mae = "Placa-mãe H610 "
+                memoria_ram = "Memória RAM 2x8GB DDR4 @3200MHz"
+                armazenamento = "SSD NVMe 1TB Kingston NV2"
+                fonte = "Fonte de 550W"
+                placa_video = "RX 6650 XT ou RTX 3060 12GB"
+            elif orcamento < 4700: #Opção R$ 4001 - R$ 4700
+                processador = "Intel Core i5 13400F"
                 placa_mae = "Placa-mãe avançada"
                 memoria_ram = "64 GB de memória RAM (4x16GB)"
                 armazenamento = "SSD de 1 TB + HD de 4 TB"
                 fonte = "Fonte de alimentação de 750W"
-                gabinete = "Gabinete premium com excelente refrigeração"
-                placa_video = "Placa de vídeo dedicada de alto desempenho"
-            else:
-                processador = "Intel Core i9-10900K ou equivalente"
-                placa_mae = "Placa-mãe compatível com o processador"
-                memoria_ram = "128 GB de memória RAM (4x32GB)"
+                placa_video = "RX 6600"
+            elif orcamento < 5000: #Opção R$ 4701 - R$ 5000
+                processador = "Ryzen 5 5600 ou Intel Core i5 12400F"
+                placa_mae = "Placa-mãe A520"
+                memoria_ram = "Memória RAM: 2x 8GB DDR4 @3200MHz "
+                armazenamento = "SSD M.2 NVMe 512GB"
+                fonte = "Fonte de alimentação de 550W"
+                placa_video = " RX 6700 XT ou RTX 3060 Ti"
+            elif orcamento < 6000: #Opção R$ 5001 - R$ 6000
+                processador = "Ryzen 5 7600 ou Intel Core i5 13600KF"
+                placa_mae = "Placa-mãe H610"
+                memoria_ram = "Memória RAM 2x8GB DDR4 @3200MHz"
                 armazenamento = "SSD de 2 TB + HD de 8 TB"
-                fonte = "Fonte de alimentação de 1000W"
-                gabinete = "Gabinete de alto desempenho com recursos avançados de refrigeração e personalização"
-                placa_video = "Placa de vídeo de alto desempenho para aplicações profissionais"
+                fonte = "Fonte de alimentação de 7500W"
+                placa_video = "RX 7900 XT ou RTX 4070 TI"
+            elif orcamento < 10000: #Opção R$ 6001 - R$ 10000
+                processador = "Ryzen 5 7600 ou Intel Core i5 13600KF"
+                placa_mae = "Placa-mãe B550"
+                memoria_ram = "Memória RAM 2x 16GB DDR4 @3200MHz ou DDR5 @5200MHz"
+                armazenamento = "SSD NVMe 1TB Kingston NV2"
+                fonte = "Fonte de alimentação de 750W"
+                placa_video = "RX 7900 XT ou RTX 4070 TI"
 
             # Verificar uso selecionado para ajustar recomendações
             uso = combo_uso.get()
             if uso == "Doméstico":
-                if orcamento > 1500:
+                if orcamento == 2000:
                     memoria_ram = "16 GB de memória RAM (2x8GB)"
                     armazenamento = "SSD de 480GB"
             elif uso == "Jogos":
@@ -70,7 +86,6 @@ def montar_computador():
                     memoria_ram = "32 GB de memória RAM (2x16GB)"
                     armazenamento = "SSD de 1 TB + HD de 2 TB"
                     fonte = "Fonte de alimentação de 600W"
-                    gabinete = "Gabinete avançado com boa refrigeração"
                     placa_video = "Placa de vídeo dedicada de médio desempenho"
             elif uso == "Profissional":
                 if orcamento > 5000:
@@ -79,8 +94,18 @@ def montar_computador():
                     memoria_ram = "128 GB de memória RAM (4x32GB)"
                     armazenamento = "SSD de 2 TB + HD de 8 TB"
                     fonte = "Fonte de alimentação de 1000W"
-                    gabinete = "Gabinete de alto desempenho com recursos avançados de refrigeração e personalização"
                     placa_video = "Placa de vídeo de alto desempenho para aplicações profissionais"
+
+            # Verificar área selecionada para ajustar recomendações
+            area = combo_area.get()
+            if area == "Design, Fotografia e Videomaker":
+                if orcamento > 2000:
+                    placa_mae = "Placa-mãe com suporte a cores RGB"
+            elif area == "Arquitetura e Engenharias":
+                if orcamento > 3000:
+                    memoria_ram = "32 GB de memória RAM (4x8GB)"
+                    armazenamento = "SSD de 1 TB + HD de 4 TB"
+                    placa_video = "Placa de vídeo dedicada de médio desempenho"
 
             # Exibir as peças recomendadas na mesma janela
             resposta = "Recomendação:\n\n" \
@@ -89,7 +114,6 @@ def montar_computador():
                        f"Memória RAM: {memoria_ram}\n" \
                        f"Armazenamento: {armazenamento}\n" \
                        f"Fonte de alimentação: {fonte}\n" \
-                       f"Gabinete: {gabinete}\n" \
                        f"Placa de vídeo: {placa_video}"
 
             label_resposta.config(text=resposta)
@@ -109,7 +133,7 @@ def montar_computador():
     janela.title("Montagem de Computador")
 
     # Definir tamanho fixo para a janela
-    janela.geometry("500x700")
+    janela.geometry("500x500")
 
     # Centralizar a janela na tela do monitor
     janela.update_idletasks()
@@ -126,11 +150,14 @@ def montar_computador():
     label_orcamento.pack()
 
     opcoes_orcamento = [
-        "Até R$ 1000",
-        "R$ 1000 - R$ 1500",
-        "R$ 1500 - R$ 2500",
-        "R$ 2500 - R$ 5000",
-        "Mais de R$ 5000"
+        "Acima de R$ 2000",
+        "R$ 2001 - R$ 2500",
+        "R$ 2501 - R$ 3000",
+        "R$ 3001 - R$ 4000",
+        "R$ 4001 - R$ 4700",
+        "R$ 4701 - R$ 5000",
+        "R$ 5001 - R$ 6000",
+        "R$ 6001 - R$ 10000"
     ]
 
     combo_orcamento = ttk.Combobox(janela, values=opcoes_orcamento)
@@ -140,7 +167,6 @@ def montar_computador():
     label_uso = tk.Label(janela, text="Qual será o uso principal do computador?")
     label_uso.pack()
 
-    var_uso = tk.StringVar()
     opcoes_uso = [
         ("Doméstico"),
         ("Jogos"),
@@ -153,7 +179,6 @@ def montar_computador():
     
     label_area = tk.Label(janela, text="Qual é a sua área de atuação? (opcional)")
     label_area.pack()    
-    var_area = tk.StringVar()
     opcoes_area = [
         ("Design, Fotografia e Videomaker"),
         ("Arquitetura e Engenharias"),
